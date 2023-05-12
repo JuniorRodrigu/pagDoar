fc14a6c09b7a213fb24dbafaf1eed76acd0a5bd4
JUNIOR RODRIGUES <juniorrodrigues1717@gmail.com>
2023-05-12 02:39:56 -0300

    Atualizar o Paga.js 2

--- src/pages/Paga.js
+++ src/pages/Paga.js
@@ -1,96 +1,87 @@
-import React, { useReducer, useState, useEffect } from "react";
+import React, { useReducer, useState, useEffect } from "react"; 
 import axios from "axios";
+import QRCode from "qrcode.react";
 
-const api = axios.create({
-  baseURL: "https://api.mercadopago.com"
-});
+const api = axios.create({ 
+  baseURL: "https://api.mercadopago.com" 
+}); 
 
-api.interceptors.request.use(async (config) => {
-  const token = process.env.REACT_APP_TOKEN_MERCADO_PAGO_PUBLIC;
+api.interceptors.request.use(async (config) => { 
+  const token = process.env.REACT_APP_TOKEN_MERCADO_PAGO_PUBLIC; 
   config.headers.Authorization = `Bearer APP_USR-8695569384609059-042822-a13531b6ad0df397a0052de6523a47b6-200617663`;
 
-  return config;
-});
+  return config; 
+}); 
 
-const formReducer = (state, event) => {
-  return {
-    ...state,
-    [event.name]: event.value
-  };
-};
+const formReducer = (state, event) => { 
+  return { 
+    ...state, 
+    [event.name]: event.value 
+  }; 
+}; 
 
-function Paga() {
-  const [formData, setFormdata] = useReducer(formReducer, {});
-  const [responsePayment, setResponsePayment] = useState(false);
-  const [linkBuyMercadoPago, setLinkBuyMercadoPago] = useState(false);
-  const [statusPayment, setStatusPayment] = useState(false);
+function Paga() { 
+  const [formData, setFormdata] = useReducer(formReducer, {}); 
+  const [responsePayment, setResponsePayment] = useState(false); 
+  const [qrCode, setQrCode] = useState(null);
+  const [copyText, setCopyText] = useState(null);
 
-  const handleChange = (event) => {
-    setFormdata({
-      name: event.target.name,
-      value: event.target.value
-    });
-  };
+  const handleChange = (event) => { 
+    setFormdata({ 
+      name: event.target.name, 
+      value: event.target.value 
+    }); 
+  }; 
 
-  const getStatusPayment = () => {
-    api.get(`v1/payments/${responsePayment.data.id}`).then((response) => {
-      if (response.data.status === "approved") {
-      }
-    });
-  };
+  const handleSubmit = (event) => { 
+    if (event) { 
+      event.preventDefault(); 
+    } 
 
-  const handleSubmit = (event) => {
-    if (event) {
-      event.preventDefault();
-    }
+    const body = { 
+      transaction_amount: 1, 
+      description: "Produto teste de desenvolvimento", 
+      payment_method_id: "pix", 
+      payer: { 
+        email: "gerson@gmail.com", 
+        first_name: "Gerson Dev", 
+        last_name: "JS python html", 
+        identification: { 
+          type: "CPF", 
+          number: "01234567890" 
+        } 
+      }, 
+      notification_url: "https://eorpjcvcjvhqnq6.m.pipedream.net" 
+    }; 
 
-    const body = {
-      transaction_amount: 1,
-      description: "Produto teste de desenvolvimento",
-      payment_method_id: "pix",
-      payer: {
-        email: "gerson@gmail.com",
-        first_name: "Gerson Dev",
-        last_name: "JS python html",
-        identification: {
-          type: "CPF",
-          number: "01234567890"
-        }
-      },
-      notification_url: "https://eorpjcvcjvhqnq6.m.pipedream.net"
-    };
+    api 
+      .post("v1/payments", body) 
+      .then((response) => { 
+        setResponsePayment(response); 
+        setQrCode(response.data.point_of_interaction.transaction_data.qr_code.image);
+        setCopyText(response.data.point_of_interaction.transaction_data.qr_code.content);
+      }) 
+      .catch((err) => { 
+        // alert(err) 
+      }); 
+  }; 
 
-    api
-      .post("v1/payments", body)
-      .then((response) => {
-        setResponsePayment(response);
-        setLinkBuyMercadoPago(
-          response.data.point_of_interaction.transaction_data.ticket_url
-        );
-      })
-      .catch((err) => {
-        // alert(err)
-      });
-  };
+  useEffect(() => { 
+    handleSubmit(); 
+  }, []); 
 
-  useEffect(() => {
-    handleSubmit();
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        {linkBuyMercadoPago && !statusPayment && (
-          <iframe
-            src={linkBuyMercadoPago}
-            width="400px"
-            height="620px"
-            title="link_buy"
-          />
+  return ( 
+    <div className="App"> 
+      <header className="App-header"> 
+        {qrCode && (
+          <div>
+            <QRCode value={qrCode} />
+            <p>Chave PIX: {copyText}</p>
+          </div>
         )}
-      </header>
-    </div>
-  );
-}
+      </header> 
+    </div> 
+  ); 
+} 
 
 export default Paga;
