d8103bee14e46c1bdf44427e58d454fbc5877fdf
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2023-05-17 06:37:15 -0300

    pg e status cpt

--- src/pages/Paga.js
+++ src/pages/Paga.js
@@ -1,103 +1,51 @@
 import React, { useReducer, useState, useEffect } from "react";
 import axios from "axios";
 import { initializeApp } from "firebase/app";
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  doc,
-  deleteDoc,
-  getDocs,
-  onSnapshot,
-} from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseApp = initializeApp({
   apiKey: "AIzaSyCEZTV3TnxRBdtx7NqzT5-4AX7zsXUZL6E",
   authDomain: "doarcao-cd553.firebaseapp.com",
   projectId: "doarcao-cd553",
 });
 
-
 const db = getFirestore(firebaseApp);
-const usersCollectionRef = collection(db, "users");
 const api = axios.create({
-  baseURL: "https://api.mercadopago.com"
+  baseURL: "https://api.mercadopago.com",
 });
 
-api.interceptors.request.use(async (config) => {
-  const token = process.env.REACT_APP_TOKEN_MERCADO_PAGO_PUBLIC;
+api.interceptors.request.use((config) => {
   config.headers.Authorization = `Bearer APP_USR-8695569384609059-042822-a13531b6ad0df397a0052de6523a47b6-200617663`;
-
   return config;
 });
 
 const formReducer = (state, event) => {
   return {
     ...state,
-    [event.name]: event.value
+    [event.name]: event.value,
   };
 };
 
 function Paga(props) {
-  const [lastUser, setLastUser] = useState(null);
-
-  // Obtém o último usuário adicionado ao Firestore
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      const unsubscribe = onSnapshot(collection(db, "users"), (snapshot) => {
-        // Ordena os documentos pelo campo "createdAt" em ordem decrescente
-        const sortedDocs = snapshot.docs.sort((a, b) => b.data().createdAt - a.data().createdAt);
-        // Obtém o último documento adicionado
-        const lastDoc = sortedDocs[0];
-        if (lastDoc) {
-          setLastUser({ ...lastDoc.data(), id: lastDoc.id });
-        }
-      });
-    }, 900); // Aguarda 900ms (0.9s) antes de buscar o último usuário adicionado
-
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }, []);
-
-  // Reducer para o estado do formulário
-  const [formData, setFormdata] = useReducer(formReducer, {});
-
-  // Estado para armazenar a resposta do pagamento
-  const [responsePayment, setResponsePayment] = useState(false);
-
-  // Estado para armazenar o link de pagamento do Mercado Pago
-  const [linkBuyMercadoPago, setLinkBuyMercadoPago] = useState(false);
-
-  // Estado para armazenar o status do pagamento
+  const [responsePayment, setResponsePayment] = useState(null);
   const [statusPayment, setStatusPayment] = useState(false);
-
-  // Estado para armazenar o valor da transação
   const [transactionAmount, setTransactionAmount] = useState(parseFloat(props.transactionAmount));
+  const [formData, setFormdata] = useReducer(formReducer, {});
+  const [linkBuyMercadoPago, setLinkBuyMercadoPago] = useState(false);
+  let intervalId;
 
-  // Handler para atualizar o estado do formulário
   const handleChange = (event) => {
     setFormdata({
       name: event.target.name,
-      value: event.target.value
+      value: event.target.value,
     });
   };
 
-  // Função para obter o status do pagamento no Mercado Pago
-  const getStatusPayment = () => {
-    api.get(`v1/payments/${responsePayment.data.id}`).then((response) => {
-      if (response.data.status === "approved") {
-        setStatusPayment(true);
-      }
-    });
-  };
-
-  // Handler para enviar a solicitação de pagamento ao Mercado Pago
   const handleSubmit = (event) => {
     if (event) {
       event.preventDefault();
     }
 
-    // Cria um objeto com os dados da transação
     const body = {
       transaction_amount: transactionAmount,
       description: "Produto teste de desenvolvimento",
@@ -108,13 +56,12 @@ function Paga(props) {
         last_name: "JS python html",
         identification: {
           type: "CPF",
-          number: "01234567890"
-        }
+          number: "01234567890",
+        },
       },
-      notification_url: "https://hooks.slack.com/mppagamentor"
+      notification_url: "https://eodvum2vysvd4fb.m.pipedream.net",
     };
 
-    // Envia a solicitação de pagamento ao Mercado Pago
     api
       .post("v1/payments", body)
       .then((response) => {
@@ -122,36 +69,58 @@ function Paga(props) {
         setLinkBuyMercadoPago(
           response.data.point_of_interaction.transaction_data.ticket_url
         );
-        setTransactionAmount(parseFloat(formData.transaction_amount));
       })
       .catch((err) => {
         console.error(err);
       });
   };
 
-  // Envia a solicitação de pagamento ao carregar o componente
+  const checkPaymentStatus = () => {
+    if (responsePayment) {
+      api.get(`v1/payments/${responsePayment.data.id}`).then((response) => {
+        if (response.data.status === "approved") {
+          setStatusPayment(true);
+          clearInterval(intervalId);
+        }
+      });
+    }
+  };
+
+  useEffect(() => {
+    intervalId = setInterval(checkPaymentStatus, 5000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [responsePayment]);
+
   useEffect(() => {
     handleSubmit();
-  }, []);
+  }, [formData]);
+
+  useEffect(() => {
+    if (statusPayment) {
+      window.location.reload();
+    }
+  }, [statusPayment]);
 
   return (
     <div className="pix">
-      <header>
+  
+   
+  
+    <header>
         {linkBuyMercadoPago && !statusPayment && (
           <iframe
             src={linkBuyMercadoPago}
             width="100%"
-            height="800px " 
-  border-radius: "0.5rem"
+            height="800px"
             title="link_buy"
           />
         )}
-        
       </header>
-     
     </div>
   );
 }
 
 export default Paga;
-
