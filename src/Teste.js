49ab5db7c692dff099eda990d3dd047e61d12e2c
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2023-05-21 02:29:33 -0300

    f

---
+++ src/Teste.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Teste() {
+  return (
+    <div>
+      <h1>Teste Component</h1>
+      <p>Este é um exemplo do componente Teste.</p>
+    </div>
+  );
+}
+
+export default Teste;
