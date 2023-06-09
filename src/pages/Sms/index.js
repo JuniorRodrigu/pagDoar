b044b552775b74c6799d580ac897e82348dd6ec2
JUNIOR RODRIGUES <juniorrodrigues1717@gmail.com>
2023-06-09 08:11:54 -0300

    Update index.js

--- src/pages/Sms/index.js
+++ src/pages/Sms/index.js
@@ -1,55 +1,36 @@
-const bodyParser = require('body-parser');
-const twilio = require('twilio');
-const express = require('express');
-const cors = require('cors');
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const app = express();
-app.use(bodyParser.json());
+const App = () => {
+  const [error, setError] = useState('');
+  const [sent, setSent] = useState(false);
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-const accountSid = 'ACca6ad5ddc85bc3e89dbf60d1cdac693a';
-const authToken = 'fa3085868cac4b335fe4dd4a1d34abc';
-const client = new twilio(accountSid, authToken);
+    try {
+      const response = await axios.post('https://apisms.vercel.app/send-sms', {
+        numero: '+5588988231924',
+        mensagem: 'teste',
+      });
 
-app.get('/', (req, res) => {
-  res.send('Página inicial');
-});
+      console.log(response.data);
+      setSent(true);
+    } catch (error) {
+      console.error(error);
+      setError(error.message);
+    }
+  };
 
-app.get('/status', (req, res) => {
-  res.send('Servidor em execução');
-});
+  return (
+    <div>
+      {error && <p>{error}</p>}
+      {sent && <p>Mensagem enviada com sucesso!</p>}
+      <form onSubmit={handleSubmit}>
+        <button type="submit">Enviar SMS</button>
+      </form>
+    </div>
+  );
+};
 
-app.post('/send-sms', async (req, res) => {
-  const { numero, mensagem } = req.body;
-
-  console.log('Request body:', req.body);
-
-  try {
-    const message = await client.messages.create({
-      body: mensagem,
-      from: '+13203773783',
-      to: numero,
-    });
-
-    console.log('Twilio response:', message);
-    res.status(200).send(message.sid);
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send(error.message);
-  }
-});
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Servidor em execução na porta ${PORT}`);
-});
+export default App;
